a8b8bada31331a6c71d2df04c42ff388fbe54276
Li <776239423@qq.com>
2022-11-29 22:02:34 +0800

    SpringCloud学习第1天

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,30 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Service
+public class OrderService {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        // 4.返回
+        order.setUser(user);
+        return order;
+    }
+}
